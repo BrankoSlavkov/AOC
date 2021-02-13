e9bf0d47b29384e77e232749a7d0d789ea50edeb
Branko Slavkov <slavkov.branko@gmail.com>
2021-02-13 01:14:01 +0100

    Day 3 Part 1 and Part 2 solved

--- AOC2020/Day3/Program.cs
+++ AOC2020/Day3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using AOC.Common;
+using System.Collections.Generic;
 
 namespace Day3
 {
@@ -6,7 +8,18 @@ namespace Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> inputs = new List<string>();
+
+            Toboggan toboggan = new Toboggan();
+            inputs.GetInputs();
+
+            Console.WriteLine($"Right 1, down 1 -> {toboggan.TotalTreesEncountered(inputs, 1)}");
+            Console.WriteLine($"Right 3, down 1 -> {toboggan.TotalTreesEncountered(inputs, 3)}");
+            Console.WriteLine($"Right 5, down 1 -> {toboggan.TotalTreesEncountered(inputs, 5)}");
+            Console.WriteLine($"Right 7, down 1 -> {toboggan.TotalTreesEncountered(inputs, 7)}");
+            Console.WriteLine($"Right 1, down 2 -> {toboggan.TotalTreesEncountered(inputs, 1, 2)}");
+
+            Console.WriteLine($"Total: {toboggan.TreesEncounteredOnEachSlopes}");
         }
     }
 }
---
+++ AOC2020/Day3/Toboggan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day3
+{
+    public class Toboggan
+    {
+        public double TreesEncounteredOnEachSlopes = 1;
+
+        public int TotalTreesEncountered(List<string> inputs, int toRight = 1, int toBottom = 1)
+        {
+            int count = 0;
+            int position = 0;
+
+            // Jump (ignore) specific number of rows
+            for (int i = toBottom; i < inputs.Count; i += toBottom)
+            {
+                string input = inputs[i];
+                position += toRight;
+
+                // When the end is reached restore position to the beginning of the list
+                if (position >= input.Length)
+                {
+                    position -= input.Length;
+                }
+
+                char c = input[position];
+
+                if (c == '#')
+                {
+                    ++count;
+                }
+            }
+
+            TreesEncounteredOnEachSlopes *= count;
+            return count;
+        }
+    }
+}
