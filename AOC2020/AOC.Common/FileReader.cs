883916f647a99157b7adc4655dcd29c81184004e
Branko Slavkov <slavkov.branko@gmail.com>
2021-02-14 19:14:11 +0100

    File reader (read empy/blank lines)

--- AOC2020/AOC.Common/FileReader.cs
+++ AOC2020/AOC.Common/FileReader.cs
@@ -32,5 +32,31 @@ namespace AOC.Common
             return inputs;
         }
 
+        public static List<T> GetInputsWithBlankLines<T>()
+        {
+            List<T> inputs = new List<T>();
+            // File should exist in project directory
+            string filePath = $"../../../inputs.txt";
+
+            if (File.Exists(filePath))
+            {
+                using var reader = File.OpenText(filePath);
+                string line = string.Empty;
+
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+
+                    //if (!string.IsNullOrWhiteSpace(line))
+                    //{
+                    T readLine = (T)Convert.ChangeType(line, typeof(T));
+                    inputs.Add(readLine);
+                    //}
+                }
+            }
+
+            return inputs;
+        }
+
     }
 }
